e88d2d82e65e9522f5f94c948975d2e0578d80ed
Huang-Liangjin <liangjin.huang@drecom.co.jp>
2015-08-27 15:58:10 +0900

    redisの導入

--- janken/web/controllers/page_controller.ex
+++ janken/web/controllers/page_controller.ex
@@ -1,7 +1,23 @@
 defmodule Janken.PageController do
+  import Exredis
   use Janken.Web, :controller
 
+  require Logger
+
   def index(conn, _params) do
+    # redis_test("user_name")
     render conn, "index.html"
   end
+
+  # defp redis_test(user_name) do
+  #   client = Exredis.start
+  #   client |> Exredis.query ["SET", "user_name", user_name]
+  #   user_name1 = client |> Exredis.query ["GET", "user_name"]
+  #   client |> Exredis.stop
+  #   Logger.info "redis:"<>user_name1
+  #   # client = Exredis.start
+  #   # client |> Exredis.query ["SET", "user_name", user_name]
+  #   # client |> Exredis.query ["GET", "user_name"]
+  #   # client |> Exredis.stop
+  # end
 end
